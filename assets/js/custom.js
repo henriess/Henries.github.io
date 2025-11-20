56a918ee4d311f9963cc6d4255b4426968747130
henriess <henrysohth@gmail.com>
2025-11-20 18:31:23 +0800

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,15 @@
+// Configure MathJax
 window.MathJax = {
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']]
   }
 };
+
+// Dynamically load the MathJax library from CDN
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+  script.async = true;
+  document.head.appendChild(script);
+})();
